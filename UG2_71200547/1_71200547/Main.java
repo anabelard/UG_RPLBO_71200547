3272aa2bed9e90d328bd4227fbeb3a564ec63d6d
anabela <anabela.rosanti@ti.ukdw.ac.id>
2022-02-16 18:42:18 +0700

    .

--- UG2_71200547/1_71200547/Main.java
+++ UG2_71200547/1_71200547/Main.java
@@ -7,15 +7,15 @@ public class Main {
         System.out.println("Kode lampu lalu lintas saat ini: ");
         String kodeLampu = System.console().readLine();
 
-        if (kodeLampu == "1") {
+        if (kodeLampu.equals("1")) {
             System.out.println("Lampu Hijau");
             M_71200547.jalan();
             P_71200547.menunggu();
-        } else if (kodeLampu == "2") {
+        } else if (kodeLampu.equals("2")) {
             System.out.println("Lampu Kuning");
             M_71200547.kurangiKecepatan();
             P_71200547.bersiap();
-        } else if (kodeLampu == "3") {
+        } else if (kodeLampu.equals("3")) {
             System.out.println("Lampu Merah");
             M_71200547.berhenti();
             P_71200547.menyeberang();
